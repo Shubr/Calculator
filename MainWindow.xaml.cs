64ac5ce3333933ce8dd905626cc3221c9c929e46
Shubr <shubhamrangra80@gmail.com>
2024-02-21 19:39:32 +1300

    ui finished

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,17 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        arthimetics arthi;
         public MainWindow()
         {
             InitializeComponent();
+            arthi = new arthimetics();
+        }
+        private void main(object sender, RoutedEventArgs e)
+        {
+            Button x = sender as Button;
+            tbxScreen.Text += x.Content;
+            arthi.main(sender, e, tbxScreen.Text);
         }
     }
 }
---
+++ arthimetics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator
+{
+    public class arthimetics
+    {
+        public void main(object sender, RoutedEventArgs s, string textBoxs)
+        {
+            Button x = sender as Button;
+            TextBox TextBox = sender as TextBox;
+            this.checkTextBox(textBoxs, x.Content.ToString());
+            
+        }
+        void checkTextBox(string textBox, string buttonPressed)
+        {
+            List<string> text = textBox.Split(' ').ToList();
+            string[] arthi = { "x", "-", "+", "/" };
+/*            if (arthi.Contains(buttonPressed))
+            {
+                for(int i = 0; i < text.Count; i++) {
+                    MessageBox.Show(text[i]);
+                }
+            }*/
+        }
+    }
+}
